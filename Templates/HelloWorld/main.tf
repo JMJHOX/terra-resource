1be7a34c9da77f86de393af0273391f570c3c6be
JMJHOX <josemiguelaparicio507@gmail.com>
2022-09-30 23:23:06 -0500

    tag

--- Templates/HelloWorld/main.tf
+++ Templates/HelloWorld/main.tf
@@ -1,20 +1,5 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
 
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+resource "azurerm_resource_group" "rg2" {
+  name     = "myTFResourceGroup2"
   location = "westus2"
 }
